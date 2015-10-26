f8ee520557d2d6327084aecf21de9bc1a5619841
Hagen Dittmer <hagen.dittmer@outlook.com>
2015-10-26 14:11:31 +1100

    Twitter hashtag update

--- app/twitter/index.js
+++ app/twitter/index.js
@@ -3,26 +3,26 @@ var Twitter = require('twitter');
 // load up the user model
 var Person = require('../models/person');
 var tweets = [
-  "BoothBot: mooooooooore jelly beans! Cheers [name]! @masteringsap #BSE2015",
-  "BoothBot: LET THERE BE CANDY. Enjoy [name]! @masteringsap #BSE2015",
-  "BoothBot: Thanks for stopping by [name] @masteringsap #BSE2015",
-  "BoothBot: Candy, candy, candy, [name], candy @masteringsap #BSE2015",
-  "BoothBot: I still have too many jelly beans [name] @masteringsap #BSE2015",
-  "BoothBot: Step 1, Survey; Step 2, Magic; Step 3, Jelly Beans for [name]! @masteringsap #BSE2015",
-  "BoothBot: Thank you [name] - enjoy your JBs @masteringsap #BSE2015",
-  "BoothBot: That ought to keep you going [name] - nice to have met you @masteringsap #BSE2015",
-  "BoothBot: Bursting at the jelly bean seams @masteringsap #BSE2015",
-  "BoothBot: [name], wonderful to have met your hand @masteringsap #BSE2015",
-  "BoothBot: Everyone! [name] has jelly beans. Just sayin' @masteringsap #BSE2015",
-  "BoothBot: Sugar hit in 5, 4, 3, 2, 1 [name]! @masteringsap #BSE2015",
-  "BoothBot: You may well win yourself an Apple Watch [name]! @masteringsap #BSE2015 #watch @apple",
-  "BoothBot: Thanks for taking our cool survey [name] (it's way cool) @masteringsap #BSE2015",
-  "BoothBot: 01000010011001010110000101101110001011010110000101110010011000010110110101100001 @masteringsap #BSE2015",
-  "BoothBot: Internet jelly beans for [name]! You're very welcome! @masteringsap #BSE2015",
-  "BoothBot: Forefront made me. Maybe thay can make you something [name]?! @masteringsap #BSE2015",
-  "BoothBot: Forefront Analytics - dispensing jelly beans over the interwebs since... a day ago @masteringsap #BSE2015",
-  "BoothBot: [name], I can see a cute tablet over the way. Could you introduce me? @masteringsap #BSE2015",
-  "BoothBot: Great to see you [name], enjoy those blue and green jules of joy @masteringsap #BSE2015"
+  "BoothBot: mooooooooore jelly beans! Cheers [name]! @masteringsap #BSE2015 #ba15",
+  "BoothBot: LET THERE BE CANDY. Enjoy [name]! @masteringsap #BSE2015 #ba15",
+  "BoothBot: Thanks for stopping by [name] @masteringsap #BSE2015 #ba15",
+  "BoothBot: Candy, candy, candy, [name], candy @masteringsap #BSE2015 #ba15",
+  "BoothBot: I still have too many jelly beans [name] @masteringsap #BSE2015 #ba15",
+  "BoothBot: Step 1, Survey; Step 2, Magic; Step 3, Jelly Beans for [name]! @masteringsap #BSE2015 #ba15",
+  "BoothBot: Thank you [name] - enjoy your JBs @masteringsap #BSE2015 #ba15",
+  "BoothBot: That ought to keep you going [name] - nice to have met you @masteringsap #BSE2015 #ba15",
+  "BoothBot: Bursting at the jelly bean seams @masteringsap #BSE2015 #ba15",
+  "BoothBot: [name], wonderful to have met your hand @masteringsap #BSE2015 #ba15",
+  "BoothBot: Everyone! [name] has jelly beans. Just sayin' @masteringsap #BSE2015 #ba15",
+  "BoothBot: Sugar hit in 5, 4, 3, 2, 1 [name]! @masteringsap #BSE2015 #ba15",
+  "BoothBot: You may well win yourself an Apple Watch [name]! @masteringsap #BSE2015 #ba15 #watch @apple",
+  "BoothBot: Thanks for taking our cool survey [name] (it's way cool) @masteringsap #BSE2015 #ba15",
+  "BoothBot: 01000010011001010110000101101110001011010110000101110010011000010110110101100001 @masteringsap #BSE2015 #ba15",
+  "BoothBot: Internet jelly beans for [name]! You're very welcome! @masteringsap #BSE2015 #ba15",
+  "BoothBot: Forefront made me. Maybe thay can make you something [name]?! @masteringsap #BSE2015 #ba15",
+  "BoothBot: Forefront Analytics - dispensing jelly beans over the interwebs since... a day ago @masteringsap #BSE2015 #ba15",
+  "BoothBot: [name], I can see a cute tablet over the way. Could you introduce me? @masteringsap #BSE2015 #ba15",
+  "BoothBot: Great to see you [name], enjoy those blue and green jules of joy @masteringsap #BSE2015 #ba15"
 ];
 
 module.exports = function(app) {
